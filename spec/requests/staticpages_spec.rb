4cfa812b1ede74b0fa80891f6dc492c10962da34
Martin Michalec <martin.michalec@yourenquirymanager.com.au>
2013-10-25 17:33:07 +1000

    updates from 25/10/2013

--- spec/requests/staticpages_spec.rb
+++ spec/requests/staticpages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Staticpages" do
+
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -8,10 +11,15 @@ describe "Staticpages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/staticpages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/staticpages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -23,7 +31,7 @@ describe "Staticpages" do
     it "should have the right title" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/staticpages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,7 +43,7 @@ describe "Staticpages" do
     it "should have the right title" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/staticpages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
