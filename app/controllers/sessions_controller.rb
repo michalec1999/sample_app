0eb83087ab7283e2fe03905e1040e32ef8d9a624
Martin Michalec <martin.michalec@yourenquirymanager.com.au>
2013-11-04 17:06:15 +1000

    Exercises 8.6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
+		#user = User.find_by(email: params[:session][:email].downcase)
+		user = User.find_by(email: params[:email].downcase)
+		if user && user.authenticate(params[:password])
 		    # Sign the user in and redirect to the user's show page.
 		    sign_in user
 		    redirect_to user
